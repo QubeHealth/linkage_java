b8df79127b9e44d335f34ad756d03f989c67ff93
Dhiraj Jadhav <tmt.9@qubehealth.com>
2024-07-25 17:26:44 +0530

    changes

--- src/main/java/com/linkage/controller/MailController.java
+++ src/main/java/com/linkage/controller/MailController.java
@@ -5,7 +5,6 @@ import java.util.HashMap;
 import java.util.List;
 import java.util.Map;
 import java.util.TreeMap;
-import java.util.UUID;
 import javax.mail.Message;
 import javax.mail.MessagingException;
 
