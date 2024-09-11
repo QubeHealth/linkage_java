12998389a428cb41e655f001f3ead4bb7c8137cf
Kunal <kunal.kamble@qubehealth.com>
2024-09-11 18:47:07 +0530

    handled new to credit

--- src/main/java/com/linkage/controller/DigitapController.java
+++ src/main/java/com/linkage/controller/DigitapController.java
@@ -73,6 +73,10 @@ public class DigitapController extends BaseController {
             Map<String, Object> creditResponse = (Map<String, Object>) digitapResponse.getData();
 
             if (creditResponse.get("result_code") != null && !creditResponse.get("result_code").equals(101)) {
+                if (creditResponse.get("result_code").equals(102)) {
+                    return response(Response.Status.FORBIDDEN,
+                            new ApiResponse<>(true, creditResponse.get("message").toString(), creditResponse));
+                }
                 return response(Response.Status.FORBIDDEN,
                         new ApiResponse<>(false, creditResponse.get("message").toString(), creditResponse));
 
