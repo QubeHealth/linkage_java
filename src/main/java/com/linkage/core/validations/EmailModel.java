ea2b91e7ee6ead30400e2a60521feafda3f721e3
Noel Pinto <noelpinto47@Noels-MacBook-Air.local>
2024-09-23 11:41:28 +0530

    updated validation

--- src/main/java/com/linkage/core/validations/EmailModel.java
+++ src/main/java/com/linkage/core/validations/EmailModel.java
@@ -2,6 +2,7 @@ package com.linkage.core.validations;
 
 import com.fasterxml.jackson.annotation.JsonProperty;
 
+import jakarta.validation.constraints.NotEmpty;
 import lombok.Data;
 
 public class EmailModel {
@@ -10,15 +11,19 @@ public class EmailModel {
     public static class sendAdBannerEmailReq {
 
         @JsonProperty("hr_name")
+        @NotEmpty(message = "HR Name is required")
         private String hrName;
 
         @JsonProperty("product_name")
+        @NotEmpty(message = "Product Name is required")
         private String productName;
 
         @JsonProperty("cluster_name")
+        @NotEmpty(message = "Cluster Name is required")
         private String clusterName;
 
         @JsonProperty("trigger_date_time")
+        @NotEmpty(message = "Trigger Date Time is required")
         private String triggerDateTime;
     }
 }
